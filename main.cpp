8a4711e65f22c553b58da27cccc030f18a859703
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-02-03 20:50:59 -0800

    Add accel

--- main.cpp
+++ main.cpp
@@ -1,3 +1,60 @@
-void setup() {}
+/*
+ * Copyright 2022, Pals for pALS, All rights reserved.
+ *
+ * Devices:
+ * 	- Arduino Uno
+ *
+ * 	- Accelerometer
+ * 	- Heart beat sensor
+ *
+ * 	- Microphone
+ * 	- Speaker
+ */
 
-void loop() {}
+#define DO_LOG true
+
+// Min and max for an accelerometer axis
+#define RAW_MIN 0
+#define RAW_MAX 1032
+
+// Take a sample to reduce noise in accelerometer reading
+#define SAMPLE_SIZE 10
+
+const int x_input = A0;
+const int y_input = A1;
+const int z_input = A2;
+
+void setup() {
+#if DO_LOG
+    Serial.begin(9600);
+    Serial.println("Logging started.");
+#endif
+}
+
+int read_axis(int axis_pin) {
+    long reading = 0;
+
+    analogRead(axis_pin);
+    delay(1);
+
+    for (int i = 0; i < SAMPLE_SIZE; i++) {
+        reading += analogRead(axis_pin);
+    }
+    return reading / SAMPLE_SIZE;
+}
+
+void loop() {
+    int raw_x = read_axis(x_input);
+    int raw_y = read_axis(y_input);
+    int raw_z = read_axis(z_input);
+
+    long scaled_x = map(raw_x, RAW_MIN, RAW_MAX, -3000, 3000);
+    long scaled_y = map(raw_y, RAW_MIN, RAW_MAX, -3000, 3000);
+    long scaled_z = map(raw_z, RAW_MIN, RAW_MAX, -3000, 3000);
+
+    float axel_x = scaled_x / 1000.0;
+    float axel_y = scaled_y / 1000.0;
+    float axel_z = scaled_z / 1000.0;
+
+    delay(200);
+}
