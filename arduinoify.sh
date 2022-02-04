e2e316f4a66775f3c89de8bba5b0cc587a5300b3
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-02-03 20:25:26 -0800

    Add start

---
+++ arduinoify.sh
@@ -0,0 +1,7 @@
+echo -e "Making \e[32m\"pals-for-pals/\"\e[0m"
+mkdir -p pals-for-pals/
+
+echo -e "Moving \e[32m\"main.cpp\" \e[0m-> \e[32m\"pals-for-pals/pals-for-pals.ino\"\e[0m"
+cp ./main.cpp pals-for-pals/pals-for-pals.ino
+
+echo -e "Done!"
